1d76bc85accdbaae8c93a0d339d320c3ed5f819a
dsjramesh7 <rameshdussa07@gmail.com>
2024-07-13 23:55:03 +0530

    practice

---
+++ Day1.js
@@ -0,0 +1,41 @@
+// task 1
+var num1 = 34;
+console.log(num1);
+
+// task 2
+let num2 = 14;
+console.log(num2);
+
+// task 3
+const zorodead = false;
+console.log(zorodead);
+
+// task 4
+const numberdataType = 45;
+const nulldataType = null;
+const undefineddataType = undefined;
+const bigIntdataType = 9007199254740991n;
+const booleandataType = true;
+const stringdataType = "hello";
+
+const arraydataType = [1, 2, "tyre", 23, true];
+const objectdataType = {
+  id: 1,
+  name: "luffy",
+  bounty: 1000000000000,
+  dead: false,
+};
+
+// task 5
+let heroName = "midoriya";
+console.log(heroName);
+heroName = "Deku";
+console.log(heroName);
+
+//task 6
+let heroName = "midoriya";
+console.log(heroName);
+heroName = "Deku";
+console.log(heroName);
+
+
